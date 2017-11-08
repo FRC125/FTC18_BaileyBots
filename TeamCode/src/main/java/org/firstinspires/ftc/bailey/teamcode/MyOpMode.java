877ab37850504b838f0699dfe58b7aa195a94dd0
unbun <uwh.547@gmail.com>
2017-11-07 22:01:18 -0500

    Initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/bailey/teamcode/MyOpMode.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.bailey.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by unnas on 10/16/2017.
+ */
+
+
+@TeleOp(name = "TeleOp", group = "TeleOP")
+
+public class MyOpMode extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+
+    }
+
+
+}
