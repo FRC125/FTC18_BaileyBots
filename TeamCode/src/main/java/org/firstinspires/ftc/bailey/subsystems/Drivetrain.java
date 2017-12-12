3bd832b7569a45d04d260249e6773ff67896c7c4
unbun <uwh.547@gmail.com>
2017-12-12 15:09:50 -0500

    subsystems with custom hardware

--- TeamCode/src/main/java/org/firstinspires/ftc/bailey/subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/bailey/subsystems/Drivetrain.java
@@ -1,67 +1,132 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.bailey.subsystems;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.bailey.hardware.BGyro;
+import org.firstinspires.ftc.bailey.hardware.BMotor;
 
-import static android.R.attr.left;
-import static android.R.attr.right;
+/**
+ * Created by Josh on 11/6/2017.
+ * unbun on 12/12/2017
+ */
 
 public class Drivetrain {
-    /* Public OpMode members. */
 
-    /* Local OpMode members. */
-    HardwareMap hwMap  = null;
-    private ElapsedTime period  = new ElapsedTime();
+    //PD constants for control g is for Gyro, k is for Drivetrain
+    private final double kP = 1.0;
+    private final double kD = 0.5;
+    private final double gP = 1.0;
+    private final double gD = 0.5;
 
-    /* Constructor */
-    public Drivetrain() {
+
+    //Motors declared
+    private BMotor leftFrontDrive;
+    private BMotor leftBackDrive;
+    private BMotor rightFrontDrive;
+    private BMotor rightBackDrive;
+
+    //Gyro declared
+    private BGyro gyro;
+
+
+    //Declaring drivetrain specifics for driving distance
+    private double wheelCircumfrence;
+    private double ticksPerRev;
+    private double feetPerTick;
+
+    //Timing for control
+    ElapsedTime timer = new ElapsedTime();
+
+    //Auto Constants
+    private double lastError = 0.;
+    private double lastTime = 0.;
+    private double relativeClicks = 0.;
+    private double headingToHold = 0.;
+    private double driveDistanceTolerance = 20;
+    private double turnToAngleTolerance = 5;
+
+
+    /**
+     * Drivetrain class used for controlling BB18 robot drivebase
+     *
+     * @param lF   left front motor of the drivetrain
+     * @param rF   right front motor of the drivetrain
+     * @param gyro gyro used for heading
+     */
+    public Drivetrain(BMotor lF, BMotor lB, BMotor rF, BMotor rB, BGyro gyro) {
+        this.leftFrontDrive = lF;
+        this.rightFrontDrive = rF;
+        this.leftBackDrive = lB;
+        this.rightBackDrive = rB;
+
+        //Drivetrain specifics initialized
+        wheelCircumfrence = Math.PI * (1 / 3); // Feet
+        ticksPerRev = leftFrontDrive.getCountsPerRev();
+        feetPerTick = wheelCircumfrence / ticksPerRev;
     }
 
-    public void init(HardwareMap ahwMap) {
-
+    private double getRelativeEncoderPos() {
+        return leftFrontDrive.getCurrentPos() + relativeClicks;
     }
 
-    public void setMotorsLeftRight(double left, double right){
-
+    public boolean distanceDrive(double ftDistance) { // REQUIRES HOLD HEADING!!!
+        double setpoint = ftDistance / feetPerTick;
+        double error = setpoint - getRelativeEncoderPos();
+        double now = timer.seconds();
+        double dt = now - lastTime;
+        lastTime = now;
+        double output = (kP * error) + ((kD * (error - lastError)) / dt);
+        tankDrive(output, output, true);
+        lastError = error;
+        error = setpoint - getRelativeEncoderPos();
+        return driveDistanceTolerance > error;
     }
 
-
-    public void setMotorsSpeedTurn(double speed, double turn){
-
+    public void resetDriveDistance() {
+        relativeClicks = leftFrontDrive.getCurrentPos();
+        lastError = 0.;
+        lastTime = 0.;
+        timer.reset();
     }
 
-    public double getEncDistance(){
-        return 0;
+    public void setAngleToHold(double angleToHold) {
+        headingToHold = angleToHold;
+    }
+
+    /**
+     * Turns robot to specified angle using simple P loop
+     *
+     * @param angleSetpoint specified angle
+     */
+    public boolean turnToAngle(double angleSetpoint) {
+        double gyroHeading = gyro.getAngle();
+        double angleDifference = angleSetpoint - gyroHeading;
+        double turn = gP * angleDifference;
+        tankDrive(turn, -turn, false);
+        return turnToAngleTolerance > angleDifference;
+    }
+
+    /**
+     * Sets power to the drivetrain thus driving it
+     *
+     * @param lPower power to the left side of the drivetrain
+     * @param rPower power to the right side of the drivetrain
+     * @param holdHeading true --> don't turm; false --> use gyro & input to turn
+     */
+    public void tankDrive(double lPower, double rPower, boolean holdHeading) {
+        if (holdHeading) {// want to turn
+            double gyroHeading = gyro.getAngle();
+            double angleDifference = headingToHold - gyroHeading;
+            double turn = gP * angleDifference;
+            lPower += turn;
+            rPower -= turn;
+        }
+        leftFrontDrive.setPower(lPower);
+        leftBackDrive.setPower(lPower);
+        rightFrontDrive.setPower(-rPower); //do you need to negate if the motor is set to reversed?
+        rightBackDrive.setPower(-rPower);
+    }
+
+    public void tankDrive(double lPower, double rPower){
+        this.tankDrive(lPower, rPower, false);
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/bailey/subsystems/Elevator.java
@@ -0,0 +1,78 @@
+package org.firstinspires.ftc.bailey.subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.bailey.hardware.BGyro;
+import org.firstinspires.ftc.bailey.hardware.BMotor;
+
+import static android.os.Build.VERSION_CODES.M;
+
+/**
+ * Created by unbun on 12/12/2017
+ */
+
+public class Elevator {
+
+
+    //need simple P to optimize elevator control
+    private final double kP = 1.0;
+
+    //Harwade declared
+    private BMotor winchMotor;
+    private TouchSensor bottomLimit;
+
+    private final double WINCH_INCREMENT = 8.0;
+    private final double MAX_HEIGHT = 32.0;
+    private final double MIN_HEIGHT = 0.0;
+    private final double POS_TOLELRANCE = 0.01;
+
+    private double relativeClicks = 0.;
+
+    //Timing for control
+    ElapsedTime timer = new ElapsedTime();
+
+    public Elevator(BMotor winch, TouchSensor touch) {
+        this.winchMotor = winch;
+        this.bottomLimit = touch;
+    }
+
+    /**
+     *@param power power to run winch
+     */
+    public void run(double power){
+        if(this.bottomLimit.isPressed()){
+            this.winchMotor.setPower(0);
+        }else {
+            this.winchMotor.setPower(power);
+        }
+    }
+
+    private double getRelativeEncoderPos() {
+        return winchMotor.getCurrentPos() + relativeClicks;
+    }
+
+    /**
+     * @param up true --> move up; false --> move down
+     */
+    public boolean changePos(boolean up){
+        double setpoint;
+
+        if(up){
+            setpoint = Math.max(MAX_HEIGHT, winchMotor.getCurrentPos() + WINCH_INCREMENT);
+        }else{
+            setpoint = Math.min(MIN_HEIGHT, winchMotor.getCurrentPos() - WINCH_INCREMENT);
+        }
+
+        double diff = setpoint - winchMotor.getCurrentPos();
+        double pow = kP * diff;
+        run(pow);
+        return POS_TOLELRANCE > Math.abs(diff); // return true when diff is w/in tolerance
+    }
+
+    public void resetWinch() {
+        relativeClicks = winchMotor.getCurrentPos();
+    }
+}
